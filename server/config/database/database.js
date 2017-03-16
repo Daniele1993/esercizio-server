4694563e0c4eecf159dd8bd3e0fe6b96444b720f
artoocampus <artoocampus@Airdiaroocampus.homenet.telecomitalia.it>
2017-03-16 15:38:41 +0100

    collegamento al db

---
+++ server/config/database/database.js
@@ -0,0 +1,10 @@
+var mongoose= require('mongoose');
+
+mongoose.connect('mongodb://admin:admin@ds133290.mlab.com:33290/dbcompleto',function(err){
+    if(err){
+        console.log(err);
+    }else{
+        console.log("database connesso");
+    }
+})
+
--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ var app=express();
 const PORT = 3000;
 
 
+require('./config/database/database.js');
+
+
 app.listen(PORT,function(){
     console.log('app start at http://localhost' + PORT);
 })
