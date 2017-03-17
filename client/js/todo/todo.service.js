640d7f507aa0b23f98681b7a32c13e5e14476fc9
artoocampus <artoocampus@Airdiaroocampus.homenet.telecomitalia.it>
2017-03-17 10:23:29 +0100

    pare che funziona... creaTodo aggiunto

--- client/js/todo/todo.service.js
+++ client/js/todo/todo.service.js
@@ -1,5 +1,40 @@
-angular.module('app').service('todoSrv',function(){
+angular.module('app').service('todoSrv',function($http){
+    
+    const PORT=3000;
+
+    var getTodo=function(){
+
+        return $http({
+            method:'GET',
+            url:'http://localhost:' + PORT + '/api/dbcompleto'
+
+        }).then(function(res){
+            return res.data;
+        },function(err){
+            
+        })
+        .catch(function(err){
+            return err;
+        })
+
+    }
+     var creaTodo=function(nuovo){
+         return $http({
+             method:'POST',
+             url:'http://localhost:' + PORT + '/api/dbcompleto',
+             data:nuovo
+         }).then(function(res){
+             return res.data;
+         },function(err){
+
+         }).catch(function(err){
+             return err;
+         });
+
+     }
+    
     return{
-        
+        getTodo:getTodo,
+        creaTodo:creaTodo
     }
 })
--- client/js/todo/todoCtrl.js
+++ client/js/todo/todoCtrl.js
@@ -1,3 +1,21 @@
-angular.module('app').controller('todoCtrl',function($scope){
-    $scope.titolo="la mia cazzo di todo list"
+angular.module('app').controller('todoCtrl',function($scope,todoSrv,$state){
+    
+    $scope.titolo="la mia cazzo di todo list";
+
+
+    $scope.lista =[];
+    todoSrv.getTodo().then(function(data){
+        $scope.lista = data;
+        
+    });
+    
+    $scope.crea=function(){
+        console.log($scope.nuovo);
+        todoSrv.creaTodo($scope.nuovo)
+               .then(function(data){
+                   console.log(data);
+                   $scope.nuovo="";
+        });
+    }
+    
 });
